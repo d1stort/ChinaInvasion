ddeba0315e4e388aba893731537a9811471131be
yanchezz <yano4ca@mail.com>
2016-01-13 14:58:30 +0300

    Simple LINQ request created

--- Team_Project/Program.cs
+++ Team_Project/Program.cs
@@ -10,7 +10,21 @@ namespace Team_Project
     {
         static void Main(string[] args)
         {
+            var songs = new List<Song>();
 
+            // Uncomment 1 item at a time and check the results
+
+            //PrintSongs(songs);
+        }
+
+        static void PrintSongs(List<Song> song)
+        {
+            var lsongs = from s in song
+                         where s.Duration > 3
+                         select s;
+
+            foreach (var ls in lsongs)
+                Console.WriteLine("{0} - {1}", ls.SongName, ls.Duration);
         }
     }
 }
